7e0f8924e7611ab32e42911c5a088a45d1abc363
user <Global@mail.ua>
2017-04-18 20:47:27 +0300

    some new in films controller and films dir

--- application/views/films/index.php
+++ application/views/films/index.php
@@ -1,8 +1,20 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<h1>Фильмография</h1>
+<table>
+    <caption>Работы</caption>
+    <tr>
+	<th>№</th>
+	<th>Название фильма</th>
+	<th>Год выпуска</th>
+	<th>Ссылка</th>
+    </tr>
+    <?php foreach ($data as $val) :?>
+    <tr>
+	<td><?=  $val['id']?></td>
+	<td><?=  $val['name']?>
+	</td>
+	<td><?=  $val['year']?></td>
+	<td><a href="<?=  'films/'.$val['id']?>"><?=  'films/'.$val['id']?></a>
+	</td>
+    </tr>
+    <?php endforeach; ?>
+</table>
