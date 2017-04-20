1593b44f27d6237b6a87ade1f0bf338579c28cc7
user <Global@mail.ua>
2017-04-20 19:04:38 +0300

    up modelFilms

--- application/models/ModelFilms.php
+++ application/models/ModelFilms.php
@@ -1,31 +1,36 @@
 <?php
+require_once '../core/config/db.php';
 
 class ModelFilms extends Model {
+
     private $db;
-    
-    public function get_data() {
+    public function __construct() {
 	$this->db = new mysqli(DB['localhost'], DB['root'], DB[''], DB['FAF']);
     }
-    
-    public function get_films(){
+
+    public function get_data() {
+
+    }
+
+    public function get_films() {
 	$query = "SELECT * FROM films;";
 	$result = $this->db->query($query);
-	if($result){
+	if ($result) {
 	    $str = '';
 	    while ($res = $result->fetch_assoc()) {
-		$str .= "<p>".$res['name']."</p>";
+		$str .= "<p>" . $res['name'] . "</p>";
 	    }
 	    return $str;
 	}
     }
-    
-    public function get_film_by_id(){
+
+    public function get_film_by_id() {
 	$query = "SELECT * FROM films ORDER BY id;";
 	$result = $this->db->query($query);
-	if($result){
+	if ($result) {
 	    $str = '';
 	    while ($res = $result->fetch_assoc()) {
-		$str .= "<p>".$res['name']."</p>";
+		$str .= "<p>" . $res['name'] . "</p>";
 	    }
 	    return $str;
 	}
