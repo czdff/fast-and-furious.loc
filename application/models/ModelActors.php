9e6c4d0197f7227c53921ff39bc88bbfbcd77c2c
Vasyl <admin@gmail.com>
2017-04-20 19:19:23 +0300

    temporary save

--- application/models/ModelActors.php
+++ application/models/ModelActors.php
@@ -1,7 +1,19 @@
 <?php
 
 class ModelActors extends Model {
+
     public function get_data() {
-	
+	$db = new mysqli(DB_, 'root', '', 'FAF');
+	if (!$db->connect_errno) {
+	    $q = "SELECT id,name,lastname FROM Actors ";
+	    $res = $db->query($q);
+	    if ($res) {
+		$actors = array();
+		while ($actor = $res->fetch_assoc()) {
+		    $actors[] = $actor;
+		}
+	    }
+	}
     }
+
 }
--- application/views/actors/index.php
+++ application/views/actors/index.php
@@ -1,8 +1,24 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$db = new mysqli('localhost', 'root', '', 'FAF');
+if (!$db->connect_errno) {
+    $q = "SELECT id,name,lastname FROM Actors ";
+    $res = $db->query($q);
+    if ($res) {
+	$actors = array();
+	while ($actor = $res->fetch_assoc()) {
+	    $actors[] = $actor;
+	}
+    }
+}
+var_dump($actors);
+echo '<table cellpadding="5" cellspacing="0" border="1"><tr><td>id</td><td>Имя</td><td>Фамилия</td><td>Подробности</td>';
 
+foreach ($actors as $one) {
+    echo '<tr>';
+    foreach ($one as $sValue) {
+	echo "<td>{$sValue}</td>";
+    }
+    echo '</tr>';
+}
+echo '</table>';
