412b1239f49dd214871d32b673bde9aaca2dc896
Someone <rgbjkbs@hdbf.ru>
2017-04-20 19:38:16 +0300

    add ModelMain.php

--- application/controllers/ControllerMain.php
+++ application/controllers/ControllerMain.php
@@ -2,6 +2,8 @@
 
 class ControllerMain extends Controller {
     public function actionIndex() {
-	
+        $this->model=new ModelMain();
+        $data=  $this->model->get_data();
+	$this->view->generate('index.php','layout.php',$data);
     }
 }
